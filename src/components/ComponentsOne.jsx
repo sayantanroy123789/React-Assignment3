74c5c4080764b85529e773e13bb1da4792e7a9f1
sayantan <sayantan.roy123789@gmail.com>
2023-08-10 20:38:27 +0530

    first commit

---
+++ src/components/ComponentsOne.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function ComponentsOne() {
+  return (
+    <div>
+      <h1>Sayantan Roy</h1>
+      <h5>Software Developer, Educator of Geekster</h5>
+      <div>
+        <a href="">Linkedin </a>
+
+        <a href="" style={{ marginLeft: "4px" }}>
+          {" "}
+          Github
+        </a>
+      </div>
+      <hr />
+      <div>
+        <h3>Exprience</h3>
+        <h4>Software Developer at Amazon</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
+          dolorem. Ut rem, vel velit consequatur iusto numquam. Officia ut,
+          perspiciatis accusamus rem voluptate nam asperiores repellendus,
+          cumque inventore explicabo corporis. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Atque exercitationem sequi velit quae
+          accusamus nostrum hic ad reiciendis illum libero.
+        </p>
+        <hr />
+        <h3>Educator at Geekster</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos et
+          nulla, dolorum temporibus quaerat odio incidunt repellendus! At
+          doloribus voluptatibus reiciendis ullam tempora, facere provident,
+          assumenda eveniet dignissimos aliquam corrupti.
+        </p>
+        <hr/>
+      </div>
+    </div>
+  );
+}
+
+export default ComponentsOne;
---
+++ src/components/ComponentsTwo.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function ComponentsTwo() {
+  return (
+    <div>
+        <div>
+            <h2>SKILL</h2>
+            <ul>
+                <li>JAVA</li>
+                <li>JAVASCRIPT</li>
+                <li>REACT JS</li>
+                <li>SYSTEM DESIGNE</li>
+            </ul>
+        </div>
+        <hr/>
+        <div>
+            <h2>HOBBIES</h2>
+            <ol>
+                <li>Playing cricket</li>
+                <li>Football</li>
+                <li>Biography</li>
+                <li>Teaching</li>
+            </ol>
+            <hr/>
+        </div>
+    </div>
+  )
+}
+
+export default ComponentsTwo
